fd791e51113539b83dd71331748953d7df7385ed
Philip Rebeschini <philiprebeschini@outlook.com>
2020-11-23 10:00:57 +0100

    Don't need all those imports.

--- InsertionXtests.java
+++ InsertionXtests.java
@@ -1,31 +1,9 @@
 package edu.princeton.cs.algs4;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Arrays;
-
-import java.util.Iterator;
 
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ArgumentsSource;
-import org.junit.jupiter.params.provider.ValueSource;
-
-import edu.princeton.cs.algs4.collections.Vector;
 import edu.princeton.cs.algs4.sorting.InsertionX;
-import net.jqwik.api.Arbitraries;
-import net.jqwik.api.Arbitrary;
-import net.jqwik.api.Assume;
 import net.jqwik.api.ForAll;
 import net.jqwik.api.Property;
-import net.jqwik.api.Provide;
-import net.jqwik.api.Report;
-import net.jqwik.api.constraints.IntRange;
-import net.jqwik.api.constraints.Positive;
-import net.jqwik.api.lifecycle.AfterProperty;
-import net.jqwik.api.lifecycle.BeforeProperty;
-import java.util.Random;
-
-import java.util.Collections.*;
 
 public class InsertionXtests {
 
